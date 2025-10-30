a51fca6529a67ee0c7d85250989a3518f2364d2d
manniknassa111-afk <man.nik.nassa111@gmail.com>
2025-10-30 21:31:26 +0500

    Refactor Web3Modal integration in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,30 @@
 import '../styles/globals.css';
-import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react'
-import { WagmiConfig } from 'wagmi'
+import { Web3Modal } from '@web3modal/react'
+import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
+import { configureChains, createClient, WagmiConfig } from 'wagmi'
 import { bsc } from 'wagmi/chains'
 
-const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID
+const chains = [bsc];
+const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
 
-const metadata = {
-  name: 'DIDIPAI Presale',
-  description: 'Token presale platform on Binance Smart Chain',
-  url: 'https://didipai-presale.vercel.app',
-  icons: ['https://didipai-presale.vercel.app/icon.png']
-}
+const { provider } = configureChains(chains, [w3mProvider({ projectId })]);
 
-const chains = [bsc]
-const wagmiConfig = defaultWagmiConfig({ chains, projectId, metadata })
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: w3mConnectors({ projectId, version: 2, chains }),
+  provider,
+});
 
-createWeb3Modal({
-  wagmiConfig,
-  projectId,
-  chains
-})
+const ethereumClient = new EthereumClient(wagmiClient, chains);
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <Component {...pageProps} />
-    </WagmiConfig>
-  )
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <Component {...pageProps} />
+      </WagmiConfig>
+
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+    </>
+  );
 }
